e23731d498a9b7fd945ff8582247cb4af4f2e250
Murylo Anjos <muryloanjos51@gmail.com>
2025-03-26 00:00:50 -0300

    Adição do Projeto Back-end ao repositório

---
+++ Back-end/projbim/projbim/src/main/java/com/bsp/projbim/ProjbimApplication.java
@@ -0,0 +1,13 @@
+package com.bsp.projbim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjbimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjbimApplication.class, args);
+	}
+
+}
---
+++ Back-end/projbim/projbim/src/test/java/com/bsp/projbim/ProjbimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsp.projbim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjbimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
